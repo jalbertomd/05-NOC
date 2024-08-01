ed25834acbabe13b92e576f18b3d589c0233ee1e
Alberto Martinez <jdiaz@revealdata.com>
2024-08-01 10:03:36 -0600

    Fin de seccion 11

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,16 +1,21 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
+
 interface CheckServiceUseCase {
     execute(url: string): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
 
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback
     ) { }
+
 
     public async execute(url: string): Promise<boolean> {
 
@@ -20,13 +25,28 @@ export class CheckService implements CheckServiceUseCase {
                 throw new Error(`Error on check service ${url}`);
             }
 
-            this.successCallback();
+            const log = new LogEntity({
+                message: `Service ${url} working`,
+                level: LogSeverityLevel.low,
+                origin: 'check-service.ts'
+            });
+
+            this.logRepository.saveLog(log);
+            this.successCallback && this.successCallback();
+
             return true;
         } catch (error) {
+            const errorMessage = `${url} is not ok. ${error}`;
+            const log = new LogEntity({
+                message: errorMessage,
+                level: LogSeverityLevel.high,
+                origin: 'check-service.ts'
+            });
+            
+            this.logRepository.saveLog(log);
 
-            console.table(`${error}`);
+            this.errorCallback && this.errorCallback(errorMessage);
 
-            this.errorCallback(`${error}`);
             return false;
         }
     }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,24 +1,47 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from './email/email.service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource(),
+);
+
+const emailService = new EmailService();
 
 export class Server {
 
-    public static start() {
+  public static start() {
 
-        console.log('Server started...');
+    console.log('Server started...');
 
-        CronService.createJob(
-            '*/10 * * * * *',
-            () => {
-              const url = 'https://google.com';
-              new CheckService(
-                () => console.table( `${ url } is ok` ),
-                ( error ) => console.table( error ),
-              ).execute( url );
-              // new CheckService().execute( 'http://localhost:3000' );
-              
-            }
-          );
-    }
+    //todo: Mandar email
+    /*new SendEmailLogs(
+      emailService,
+      fileSystemLogRepository,
+    ).execute(
+      ['j.alberto.martinez.d@gmail.com', 'jay_eym@hotmail.com']
+    )
+    /*emailService.sendEmailWithFileSystemLogs(
+      ['j.alberto.martinez.d@gmail.com', 'jay_eym@hotmail.com']
+    );
+    //*/
+
+    //CronService.createJob(
+    //'*/10 * * * * *',
+    //() => {
+    //const url = 'http://localhost:3000';
+    //new CheckService(
+    //fileSystemLogRepository,
+    //  () => console.table(`${url} is ok`),
+    //    (error) => console.table(error),
+    //    ).execute(url);
+    //      // new CheckService().execute( 'http://localhost:3000' );
+    //
+    //  }
+    //);
+  }
 }
 
