2f9f3dbd8a8df3090e4c46394bd692f3c1e1d026
Alberto Martinez <jdiaz@revealdata.com>
2024-08-01 16:11:22 -0600

    Final de seccion 11

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
 import { envs } from "./config/plugins/env.plugin";
+import { LogModel, MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
 
 (async () => {
     main();
 })();
 
-function main() {
+async function main() {
+
+    // await MongoDatabase.connect({
+    //     mongoUrl: envs.MONGO_URL,
+    //     dbName: envs.MONGO_DB_NAME,
+    // });
+
     Server.start();
     //console.log(envs);
+
+    // Crear una coleccion = tables, documento = registro
+    /*const newLog = await LogModel.create({
+        message: 'Test message desde Mongo',
+        origin: 'App.ts',
+        level: 'low'
+    });
+
+    await newLog.save();
+    console.log(newLog);*/
+
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,12 +1,27 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from './email/email.service';
 
-const fileSystemLogRepository = new LogRepositoryImpl(
+const LogRepository = new LogRepositoryImpl(
+  //new FileSystemDatasource(),
+  //new MongoLogDatasource(),
+  new PostgresLogDatasource(),
+);
+
+const fsLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource(),
+);
+const mongoLogRepository = new LogRepositoryImpl(
+  new MongoLogDatasource(),
+);
+const postgresLogRepository = new LogRepositoryImpl(
+  new PostgresLogDatasource(),
 );
 
 const emailService = new EmailService();
@@ -29,19 +44,17 @@ export class Server {
     );
     //*/
 
-    //CronService.createJob(
-    //'*/10 * * * * *',
-    //() => {
-    //const url = 'http://localhost:3000';
-    //new CheckService(
-    //fileSystemLogRepository,
-    //  () => console.table(`${url} is ok`),
-    //    (error) => console.table(error),
-    //    ).execute(url);
-    //      // new CheckService().execute( 'http://localhost:3000' );
-    //
-    //  }
-    //);
+    // CronService.createJob(
+    //   '*/10 * * * * *',
+    //   () => {
+    //     const url = 'https://www.google.com';
+    //     new CheckServiceMultiple(
+    //       [LogRepository, mongoLogRepository, postgresLogRepository],
+    //       () => console.table(`${url} is ok`),
+    //       (error) => console.table(error),
+    //     ).execute(url);
+    //   }
+    // );
   }
 }
 
